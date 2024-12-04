2b57736745d6f1c2b948198cf902d1f06cc0adde
rmh <rmh63@naver.com>
2024-12-04 18:19:31 +0900

    패키지 변경

---
+++ money-service/src/main/java/com/pay/money/adapter/out/service/banking/BankingInfo.java
@@ -0,0 +1,15 @@
+package com.pay.money.adapter.out.service.banking;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BankingInfo {
+    private String membershipId;
+    private String bankName;
+    private String bankAccountNumber;
+    private boolean isValid;
+}
