e8efda13c5f94b39f0ae9a7b314882e1d77f4952
rmh6363 <72856920+rmh6363@users.noreply.github.com>
2024-12-19 14:33:59 +0900

    Update RegisteredMembershipAggregate.java

--- membership-service/src/main/java/com/pay/membership/adapter/axon/aggregate/RegisteredMembershipAggregate.java
+++ membership-service/src/main/java/com/pay/membership/adapter/axon/aggregate/RegisteredMembershipAggregate.java
@@ -19,7 +19,7 @@ import static org.axonframework.modelling.command.AggregateLifecycle.apply;
 @Aggregate
 @Data
 public class RegisteredMembershipAggregate {
-    //test17
+
     @AggregateIdentifier
     private String id;
 
