ad5a917370cdea89c580a9f1967f642780dcd79a
rmh6363 <72856920+rmh6363@users.noreply.github.com>
2024-12-19 13:07:08 +0900

    Update SwaggerConfig.java

--- membership-service/src/main/java/com/pay/membership/SwaggerConfig.java
+++ membership-service/src/main/java/com/pay/membership/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.pay.membership"))
                 .paths(PathSelectors.any())
-                .build();
+                 .build();
     }
 
     private ApiInfo apiInfo() {
